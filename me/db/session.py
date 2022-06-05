8f439cc233f72f97d02a6b013c70c25425185503
DumbassNamedTuna <50528857+DumbassNamedTuna@users.noreply.github.com>
2022-06-04 22:27:57 -0700

    Change from FastAPI to Django

--- me/db/session.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, Session
-
-from me.core.settings import settings
-
-engine = create_engine(url = settings.pg_dsn, pool_pre_ping = True)
-LocalSession: Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- wsgi.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from me.core import security
-
-# app = FastAPI()
-
-
-# @app.get("/")
-# async def root():
-#     return {"message": "Hello World"}
