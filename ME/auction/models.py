d59b3974cd128a6dd8b73d24398892c2695d222d
DumbassNamedTuna <50528857+DumbassNamedTuna@users.noreply.github.com>
2022-06-23 17:05:42 -0700

    Updates to models.py in core and auction apps

--- ME/auction/models.py
+++ ME/auction/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+import ME.core.models
 
-# Create your models here.
+
+class Auction(models.Model):
+    
+    id = models.AutoField(primary_key = True)
+
+    starts_at = models.DateTimeField()
+    ends_at = models.DateTimeField()
+
+class Bid(models.Model):
+
+    id = models.AutoField(primary_key = True)
+
+    user = models.OneToOneField(
+        ME.core.models.User,
+        related_name = "auction",
+        on_delete = models.CASCADE,
+        primary_key = False
+    )
+    auction = models.ForeignKey(Auction, related_name = "bids", on_delete = models.CASCADE)
+    value = models.IntegerField()
+
+    created_at = models.DateTimeField(auto_now_add = True)
--- ME/core/models.py
+++ ME/core/models.py
@@ -1,8 +1,43 @@
 from django.db import models
+import ME.auction.models
 
 class User(models.Model):
+
+    id = models.AutoField(primary_key = True)
 
     email = models.EmailField(unique = True)
     username = models.CharField(max_length = 30, unique = True, null = False)
     password = models.CharField(max_length = 30, unique = False, null = False)
-    profile_picture = models.Fil
+    profile_picture = models.ImageField(upload_to = "pfp", null = True)
+    currency = models.BigIntegerField(default = 1000)
+
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+
+    def __str__(self):
+        return self.username
+
+    def __repr__(self):
+        return f"<User {self.id}>"
+
+class Meme(models.Model):
+    
+    id = models.AutoField(primary_key = True)
+    owner = models.ForeignKey(User, related_name = "memes", on_delete = models.SET_NULL)
+    auction = models.ForeignKey(ME.auction.models.Auction, related_name = "memes", on_delete = models.SET_NULL)
+
+    name = models.CharField(max_length = 50, null = False)
+    description = models.CharField(max_length = 200, default = "")
+    likes = models.IntegerField(default = 0)
+    views = models.IntegerField(default = 0)
+    price = models.IntegerField(default = 0)
+    file = models.FileField(upload_to = "memes", null = False)
+
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+
+    def __str__(self):
+        return self.name
+    
+    def __repr__(self) -> str:
+        return f"<Meme {self.id}"
