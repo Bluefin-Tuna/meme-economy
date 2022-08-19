21087b783972d0474b193bee2c2bf9c703580221
DumbassNamedTuna <50528857+DumbassNamedTuna@users.noreply.github.com>
2022-08-18 23:01:10 -0400

    Change to disable CSRF protection (will change once in production)

--- ME/ME/urls.py
+++ ME/ME/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
 from graphene_file_upload.django import FileUploadGraphQLView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('graphql', FileUploadGraphQLView.as_view(graphiql=True)),
+    path('graphql', csrf_exempt(FileUploadGraphQLView.as_view(graphiql=True))),
 ]
