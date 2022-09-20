34e3a5ca8e1c00b3c562b9975adba2b061097b61
DumbassNamedTuna <50528857+DumbassNamedTuna@users.noreply.github.com>
2022-09-20 01:55:15 -0400

    Update user.py

--- ME/core/GQL/queries/user.py
+++ ME/core/GQL/queries/user.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 import graphene
 from core.models import User
 from core.GQL.types import UserType
@@ -6,9 +7,13 @@ class UserQuery(graphene.ObjectType):
 
     all_users = graphene.List(UserType)
     user = graphene.Field(UserType, id = graphene.Int())
+    sign_in = graphene.Field(UserType, username = graphene.String(), password = graphene.String())
 
     def resolve_all_users(self, info, **kwargs):
         return User.objects.all()
     
     def resolve_user(self, info, id):
         return User.objects.get(id = id)
+    
+    def resolve_signin(self, info, username, password):
+        return get_object_or_404(User, username = username, password = password)
